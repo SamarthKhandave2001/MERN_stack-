712ac5f1a8c4b472aa1922ab8b1f540d470d4b66
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-02-18 12:27:19 +0530

    Another way to write same code

--- Javascript/part1/operation.js
+++ Javascript/part1/operation.js
@@ -1,2 +1,7 @@
 let addtion = 4 +5;
 console.log(addtion); // Output: 9
+
+let score =102;
+let bonus =20;
+let totalScore = score + bonus;
+console.log(totalScore); // Output: 122 another way to write the same code
