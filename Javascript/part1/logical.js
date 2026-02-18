a40945ad5f01dc8dc3f38a97719a06f610f3979c
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-02-18 12:42:08 +0530

    or example

--- Javascript/part1/logical.js
+++ Javascript/part1/logical.js
@@ -6,3 +6,7 @@
 let isLoggedIn = true;
 let ispaid = false;
 console.log(isLoggedIn && ispaid); // Output: false
+
+let isEmailUser = true;
+let isGoogleUser = false;
+console.log(isEmailUser || isGoogleUser); // Output: true
