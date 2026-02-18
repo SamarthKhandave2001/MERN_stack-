aa2b0b5b858e4b1183df148d7d6f816566258d63
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-02-18 12:30:16 +0530

    cover math's small operations

--- Javascript/part1/operation.js
+++ Javascript/part1/operation.js
@@ -5,3 +5,15 @@ let score =102;
 let bonus =20;
 let totalScore = score + bonus;
 console.log(totalScore); // Output: 122 another way to write the same code
+
+
+let subtract = 10 - 5;
+console.log(subtract); // Output: 5
+
+let multiplication = 4 * 5;
+console.log(multiplication); // Output: 20  
+let division = 20 / 4;
+console.log(division); // Output: 
+
+let reminder = 10 % 3;
+console.log(reminder); // Output: 1
