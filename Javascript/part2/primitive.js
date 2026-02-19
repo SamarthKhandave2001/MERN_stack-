f85eee9de05d6202a5205adead57b8d2cef80af3
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-02-19 12:42:23 +0530

    null and undefine example

--- Javascript/part2/primitive.js
+++ Javascript/part2/primitive.js
@@ -2,3 +2,19 @@
 
 let balance = 100;
 console.log(typeof balance); //number
+
+let anotherBalance = new Number(120);
+console.log(anotherBalance); //[Number: 120]
+
+//boolean
+let isActive = true;
+let isReallyActive = new Boolean(true);
+
+
+//null and undefined
+let firstname
+console.log(firstname); //undefined 
+
+//eg of null
+let lastname = null;
+console.log(lastname); //null
