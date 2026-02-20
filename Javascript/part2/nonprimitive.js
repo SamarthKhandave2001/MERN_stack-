5e33f82044186cfbcc9adb5159c84c32316f41e6
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-02-20 11:57:13 +0530

    access last name

--- Javascript/part2/nonprimitive.js
+++ Javascript/part2/nonprimitive.js
@@ -2,10 +2,14 @@
 //array, functions, objects
 
 let userName = {
-    "first name": "Sam", isLoggedin: true}; //object
+    "first name": "Sam",
+    lastName : "Appa",
+    isLoggedin: true}; //object
 
 console.log(typeof userName); //object
 console.log(userName); //{}
 
 console.log(userName["first name"]); //Sam
+
+console.log(userName.lastName); //Appa
 
