856ef9a3eb5c96cac1df9453eceedc668cdb5fb4
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-02-20 11:56:08 +0530

    example accessing the first name using obj

--- Javascript/part2/nonprimitive.js
+++ Javascript/part2/nonprimitive.js
@@ -1,7 +1,11 @@
 //Non-Primitive:
 //array, functions, objects
 
-let userName = {fiestname: "Sam", isLoggedin: true}; //object
+let userName = {
+    "first name": "Sam", isLoggedin: true}; //object
 
 console.log(typeof userName); //object
 console.log(userName); //{}
+
+console.log(userName["first name"]); //Sam
+
