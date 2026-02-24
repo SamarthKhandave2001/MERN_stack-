65b4319fe4002fc052e044757ed1e83dc019649b
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-02-24 12:38:27 +0530

    answe first

--- Javascript/part4/arrayChallenges.js
+++ Javascript/part4/arrayChallenges.js
@@ -2,3 +2,7 @@
 1. Declare an array named 'teaFlavours' that contanins the strings "Green Tea", "Black Tea", and "Oolong Tea".
 Access the first element of the array and store it in variable named 'firstTea'.
 */
+
+let teaFlavours = ["Green Tea", "Black Tea", "Oolong Tea"];
+let firstTea = teaFlavours[0];
+console.log(firstTea);
