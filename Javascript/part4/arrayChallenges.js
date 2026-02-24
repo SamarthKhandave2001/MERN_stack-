37eb5a7f1d5d93ba9edbb7bbe86e56a962b419f3
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-02-24 12:43:05 +0530

    Second question citiy addition

--- Javascript/part4/arrayChallenges.js
+++ Javascript/part4/arrayChallenges.js
@@ -6,3 +6,6 @@ Access the first element of the array and store it in variable named 'firstTea'.
 let teaFlavours = ["Green Tea", "Black Tea", "Oolong Tea"];
 let firstTea = teaFlavours[0];
 console.log(firstTea);
+
+/* 2.Declare an array named 'cities' containing "London", "Paris", "New York", and "Tokyo".
+Access the third element in the arrya and store it in avariable named 'favouriteCity'.*/
