341760eeb4a6b5314501f622788f4e1ae17517ab
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-03-02 12:33:13 +0530

    do while 3rd answer

--- Javascript/part5/loopChallenges.js
+++ Javascript/part5/loopChallenges.js
@@ -21,3 +21,14 @@ while (j >0){
 console.log(countdown);
 
 /*3. Write a 'do while' loop that prompts a user to enter their favourite tea type until they enter 'stop'. store each tea type in an array named 'teaCollection'.*/
+
+/*let teaCollection = []
+let tea
+do{
+  tea =prompt(`Enter your favourite tea (type "stop" to finish):`);
+  if(tea !== "stop"){
+    teaCollection.push(tea);
+  }
+} while (tea !== "stop");
+
+Code will work in browser environment, but not in node.js environment.*/
