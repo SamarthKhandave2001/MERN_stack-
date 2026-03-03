21a55d4667c4eefb634a00ce9825e2b9cce23fb3
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-03-03 12:48:23 +0530

    First answer

--- Javascript/part5/challenges.js
+++ Javascript/part5/challenges.js
@@ -1,2 +1,12 @@
 /* Write a 'for'loops through the array ["Greeen tea","blacck tea","chai","oolong tea""] and stops the loop when it finds "chai"".
 Store all teas before "chai""in a new array named 'selectesTeas".*/
+let teas = ["Green tea","black tea","chai","oolong tea"];
+let selectedTeas = [];
+for(let i =0; i< teas.length; i++)
+    {
+        if(teas[i] === "chai"){
+            break;
+        }
+        selectedTeas.push(teas[i]);
+    }
+console.log(selectedTeas);
