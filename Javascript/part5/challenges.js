0028b4d92a8c1c37b783418170dbe05af3087448
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-03-03 12:42:19 +0530

    first question

---
+++ Javascript/part5/challenges.js
@@ -0,0 +1,2 @@
+/* Write a 'for'loops through the array ["Greeen tea","blacck tea","chai","oolong tea""] and stops the loop when it finds "chai"".
+Store all teas before "chai""in a new array named 'selectesTeas".*/
