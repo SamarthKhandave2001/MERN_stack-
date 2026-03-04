0034840813c0bfc01eba2dd381b06e4f2e12bcc2
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-03-04 15:19:56 +0530

    function first example

--- Javascript/part6/function.js
+++ Javascript/part6/function.js
@@ -1,1 +1,7 @@
 /* 1. Write a function named 'makeTea' that takes one parameter 'typeOfTea', and returns a string like "Making green tea" when called with "green tea". Store the result in a variable named 'teaorder'.*/
+
+function greet(name){
+    console.log(`Hello, ${name}!`);
+}
+
+greet("Samarth")
