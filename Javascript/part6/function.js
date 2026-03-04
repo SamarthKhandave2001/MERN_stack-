ce6db119eb48add8e7da45d216010537a4e764f9
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-03-04 15:24:14 +0530

    function addition

--- Javascript/part6/function.js
+++ Javascript/part6/function.js
@@ -1,7 +1,7 @@
 /* 1. Write a function named 'makeTea' that takes one parameter 'typeOfTea', and returns a string like "Making green tea" when called with "green tea". Store the result in a variable named 'teaorder'.*/
-
-function greet(name){
-    console.log(`Hello, ${name}!`);
+function makeTea(typeOfTea){
+    return `Making ${typeOfTea}`;
 }
-
-greet("Samarth")
+let greet = makeTea("green tea");
+console.log(greet);
+ 
