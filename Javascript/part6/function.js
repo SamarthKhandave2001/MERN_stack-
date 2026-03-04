eeaf3ba03a876e8b6b9949d29eb9f010df544934
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-03-04 12:43:57 +0530

    function first question

---
+++ Javascript/part6/function.js
@@ -0,0 +1,1 @@
+/* 1. Write a function named 'makeTea' that takes one parameter 'typeOfTea', and returns a string like "Making green tea" when called with "green tea". Store the result in a variable named 'teaorder'.*/
