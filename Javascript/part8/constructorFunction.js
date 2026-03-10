bc9115d58b48b6789516beeadcf4e8f1ceec6471
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-03-10 19:09:40 +0530

    object creation

--- Javascript/part8/constructorFunction.js
+++ Javascript/part8/constructorFunction.js
@@ -1,3 +1,15 @@
 function Person(name,age){
-    
+    this.name = name
+    this.age = age
 }
+
+function Car(make,model){
+    this.make = make
+    this.model = model
+}
+
+let myCar = new Car("Tesla","Model S");
+console.log(myCar);
+
+let myNewCar = new Car("BMW","X5");
+console.log(myNewCar);
