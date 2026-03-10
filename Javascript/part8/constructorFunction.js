b16394ef284c30c0488300e0e0f591bfe2a81079
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-03-10 19:10:35 +0530

    comment

--- Javascript/part8/constructorFunction.js
+++ Javascript/part8/constructorFunction.js
@@ -7,7 +7,7 @@ function Car(make,model){
     this.make = make
     this.model = model
 }
-
+// If you want to crate an object then you need to use the new keyword and call the constructor function
 let myCar = new Car("Tesla","Model S");
 console.log(myCar);
 
