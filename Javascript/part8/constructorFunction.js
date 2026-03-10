740f94ae301df57fe0b61cfd5a271a58155e18a6
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-03-10 19:15:47 +0530

    new addition

--- Javascript/part8/constructorFunction.js
+++ Javascript/part8/constructorFunction.js
@@ -13,3 +13,17 @@ console.log(myCar);
 
 let myNewCar = new Car("BMW","X5");
 console.log(myNewCar);
+
+
+function Tea(type){
+    this.type = type
+    this.describe = function(){
+        return `This is a ${this.type} tea.`
+    //If you want to access any type of property or method inside the constructor function then you need to use the this keyword
+    }
+
+
+}
+
+let lemonTea = new Tea("Lemon");
+console.log(lemonTea.describe());
