81bbbf156257ebedfc14d28a0f28e3be8b63e284
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-03-11 18:50:14 +0530

    opp example

---
+++ Javascript/part9/oop-master.js
@@ -0,0 +1,10 @@
+let car = {
+    make: "Tesla",
+    model: "Model S",
+    year: 2020,
+    start: function
+    (){
+        return ` ${this.make} car got started in ${this.year} `
+    }
+}   
+console.log(car.start());
