c109cf50c5639f5a035d734b9089d09559071c98
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-03-17 12:43:03 +0530

    Polymorphism example

--- Javascript/part9/oop-master.js
+++ Javascript/part9/oop-master.js
@@ -105,3 +105,20 @@ console.log(myMachine.pressStartButton())
 
 // Polymorphism
 // It describe ability of something to have or to be displayed in more than one form
+
+class Bird{
+  fly(){
+    return ` Fyling..`
+  }
+}
+
+class Penguin extends Bird {
+  fly(){
+    return`Penguins can't fly`;
+  }
+}
+
+let bird = new Bird()
+let penguin = new Penguin()
+console.log(bird.fly());
+console.log(penguin.fly());
