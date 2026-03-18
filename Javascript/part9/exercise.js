f58ff54c1bfbde8258c3b123d4432bdcb0389a19
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-03-18 15:27:38 +0530

    Second question

--- Javascript/part9/exercise.js
+++ Javascript/part9/exercise.js
@@ -25,3 +25,14 @@ Dog.prototype.bark = function() {
 };
  
 Dog.prototype.constructor = Dog;
+
+/* Functional Constructor and Errors
+Task 1: Create a Functional Constructor
+
+Create a functional constructor Person that takes name and age as parameters. Add a method greet() to the constructor that returns "Hello, my name is [name]".
+
+
+
+Task 2: Handle Errors
+
+Modify the Person constructor to throw an error if the age is not a positive number. */
