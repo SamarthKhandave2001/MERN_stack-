1f07f7a1f7eb78b3f2f0ac6c5e7726d82653b3ce
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-03-25 15:22:03 +0530

    ui text chages using the onclick

--- Javascript/part10/newscript.js
+++ Javascript/part10/newscript.js
@@ -1,3 +1,8 @@
 //example 1
+document.getElementById("changeTextButton")
+.addEventListener('click', function (){
+    let paragraph = document.getElementById("myParagraph")
+    paragraph.textContent = "The paragraph is changed"
+})
 
-document.querySelectorAll
+
