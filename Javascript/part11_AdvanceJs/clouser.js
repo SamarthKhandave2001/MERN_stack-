d1ad21a57c7fa593feaab0bfd7de581e47433194
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-04-01 12:08:52 +0530

    defination add

--- Javascript/part11_AdvanceJs/clouser.js
+++ Javascript/part11_AdvanceJs/clouser.js
@@ -7,3 +7,6 @@ function outer(){
 
 let increment = outer()
 console.log(increment()) // 5
+
+
+/* A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives a function access to its outer scope. In JavaScript, closures are created every time a function is created, at function creation time. */
