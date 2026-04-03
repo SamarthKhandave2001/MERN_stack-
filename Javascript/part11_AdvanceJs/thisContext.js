e9f783cce2cfa9fce7d73373c8a92ebe06873746
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-04-03 15:21:05 +0530

    context code upgrade

--- Javascript/part11_AdvanceJs/thisContext.js
+++ Javascript/part11_AdvanceJs/thisContext.js
@@ -4,3 +4,10 @@ const person= {
         console.log(`Hi I am ${this.name}`);
     }
 }
+
+person.greet();
+const greetFunction= person.greet;
+person.greet()
+
+const boundGreet = person.greet.bind({name:"sam"});
+boundGreet();
