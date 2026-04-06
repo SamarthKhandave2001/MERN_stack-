5308de36003e2cc3ce0d7ad8c021476d04febd14
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-04-06 15:29:07 +0530

    comment data part add

--- Javascript/part11_AdvanceJs/asyncAwaitpart2.js
+++ Javascript/part11_AdvanceJs/asyncAwaitpart2.js
@@ -6,10 +6,24 @@ function fetchPostData() {
     });
 }
 
+
+
 function fetchCommwentData() {
      return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve("Post data fetched successfully!");
-        },2000);
+        },3000);
     });
+}
+
+async function getBlogData(){
+    try{
+console.log("Fetching blog data...");
+const blogData = await fetchPostData();
+const commentData = await fetchCommwentData();
+console.log(blogData);
+console.log(commentData);
+    }catch(error){
+        console.error("Error fetching data:", error);
+    }
 }
