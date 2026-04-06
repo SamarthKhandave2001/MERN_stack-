3cc716c577b6a2bb5f9a419f0df2565a08d0ded9
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-04-06 15:26:11 +0530

    addition

--- Javascript/part11_AdvanceJs/asyncAwaitpart2.js
+++ Javascript/part11_AdvanceJs/asyncAwaitpart2.js
@@ -5,3 +5,11 @@ function fetchPostData() {
         },2000);
     });
 }
+
+function fetchCommwentData() {
+     return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Post data fetched successfully!");
+        },2000);
+    });
+}
