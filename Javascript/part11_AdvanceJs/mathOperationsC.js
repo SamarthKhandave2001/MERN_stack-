a221e0ac20b8d15c3c97befba1e8502ea85b7de9
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-04-07 12:32:33 +0530

    app c

--- Javascript/part11_AdvanceJs/mathOperationsC.js
+++ Javascript/part11_AdvanceJs/mathOperationsC.js
@@ -1,1 +1,17 @@
 //named export 
+function add(a,b){
+    return a+b;
+
+}
+
+function substract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}   
+
+module.exports = {
+    add, substract, multiply
+};
