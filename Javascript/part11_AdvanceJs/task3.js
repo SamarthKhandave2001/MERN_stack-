aba277c8bd4c83508882ed0492dabb8711681e04
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-04-08 12:27:01 +0530

    task 3

---
+++ Javascript/part11_AdvanceJs/task3.js
@@ -0,0 +1,12 @@
+/*Prototypal Inheritance in JavaScript
+Task 1: Create Inheritance Using Prototypes
+
+Create a constructor Animal with a method makeSound(). Then create a constructor Dog that inherits from Animal and adds a method bark()
+
+
+
+Task 2: Shape and Rectangle Inheritance
+
+Create a constructor function Shape that takes color as a parameter and has a method getColor() that returns the color.
+
+Create another constructor Rectangle that inherits from Shape and adds properties width and height. Add a method getArea() to Rectangle that returns the area of the rectangle.*/
