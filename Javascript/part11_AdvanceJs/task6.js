67d1ea77d6dc62a352d4d6e7fb7e55afe3fcee0d
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-04-08 12:46:07 +0530

    task 6

---
+++ Javascript/part11_AdvanceJs/task6.js
@@ -0,0 +1,12 @@
+/* 6 - Iterators and Generators in JavaScript
+Task 1: Creating a Simple Generator
+
+Create a generator function numberGenerator() that yields numbers from 1 to 3.
+
+
+
+Task 2: Create a Custom Iterator
+
+Create a custom iterator called rangeIterator(start, end) that returns an object that iterates over numbers from start to end.
+
+Each call to .next() should return the next number in the range until it reaches end. */
