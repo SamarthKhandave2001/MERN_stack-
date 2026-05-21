0760485476918313ab2f80ac62a8a403b08b786c
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-05-21 12:34:10 +0530

    append

--- React/01basicreact/customReact/cutomreact.js
+++ React/01basicreact/customReact/cutomreact.js
@@ -3,6 +3,7 @@ const domElement =document.createElement(reactElement.type)
 domElement.innerHTML = reactElement.children
 domElement.setAttribute('href', reactElement.props.href)
 domElement.setAttribute('target', reactElement.props.target)
+container.appendChild(domElement)
 
 }
 
