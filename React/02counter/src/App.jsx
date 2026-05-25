9fdf7fbb4881b30a1f7f71fe94d6c51a5d886480
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-05-25 17:19:59 +0530

    Adding counter

--- React/02counter/src/App.jsx
+++ React/02counter/src/App.jsx
@@ -7,13 +7,16 @@ function App() {
   const [count, setCount] = useState(0)
 
 let counter = 15
-
+const addValue = () => {
+  counter = counter + 1
+  console.log(counter)
+}
   return (
     <>
       <h1>React Learning {counter} </h1>
       <h2> Counter Value {counter}</h2>
       <button>Remove Value</button>  {"   "}
-      <button>Add Value</button>
+      <button onClick={addValue}>Add Value</button>
       <p> Footer: {counter} </p>
     </>
   )
