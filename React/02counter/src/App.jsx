049f34e2d5c6d042d8d10b60826c8a7d054f244b
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-05-25 15:47:49 +0530

    counter

--- React/02counter/src/App.jsx
+++ React/02counter/src/App.jsx
@@ -6,11 +6,13 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+let counter = 15
+
   return (
     <>
       <h1>React Learning </h1>
       <h2> Counter Value</h2>
-      <button>Remove Value</button>
+      <button>Remove Value</button>  {"   "}
       <button>Add Value</button>
       <p> Footer: </p>
     </>
