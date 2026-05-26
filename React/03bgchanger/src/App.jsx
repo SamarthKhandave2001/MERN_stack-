0fb4cc5a41664581256ef6b7687ebeffe94bff62
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-05-26 16:18:23 +0530

    bg color done

--- React/03bgchanger/src/App.jsx
+++ React/03bgchanger/src/App.jsx
@@ -4,17 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('olive')
 
   return (
     <>
-    <div className= 'w-full h-screen duration-300 bg-black' > 
+    <div className= 'w-full h-screen duration-300 bg-black' style={{backgroundColor: "olive"}}> 
     <div className='fixed flex flex-wrap justify-center bottom-12 inset-x px-2'>
   <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>  
 
-<button className='outline-none px-4 py-1 rounded-full text-white shadow-lg text-black'> test</button>
+<button className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'> test</button>
+
+<button className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'> test2</button>
 
 
+<button className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'> test3</button>
 
 </div>
       </div>
