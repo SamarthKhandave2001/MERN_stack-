cdbeebd848dcbe80e5617c0aca2130dc61adcb44
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-05-27 12:08:16 +0530

    color

--- React/03bgchanger/src/App.jsx
+++ React/03bgchanger/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-    <div className= 'w-full h-screen duration-300 bg-black' style={{backgroundColor: "olive"}}> 
+    <div className= 'w-full h-screen duration-300 bg-black' style={{backgroundColor: color}}> 
     <div className='fixed flex flex-wrap justify-center bottom-12 inset-x px-2'>
   <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>  
 
