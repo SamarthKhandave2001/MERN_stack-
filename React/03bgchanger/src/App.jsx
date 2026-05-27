e5648242af6fc2a2cef24d44c4ab7f847fd1bbff
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-05-27 12:18:00 +0530

    set background color using first button

--- React/03bgchanger/src/App.jsx
+++ React/03bgchanger/src/App.jsx
@@ -5,7 +5,9 @@ import './App.css'
 
 function App() {
   const [color, setColor] = useState('olive')
-function changeColor(){ }
+function changeColor(color){
+  setColor(color)
+ } 
 
   return (
     <>
@@ -13,7 +15,7 @@ function changeColor(){ }
     <div className='fixed flex flex-wrap justify-center bottom-12 inset-x px-2'>
   <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>  
 
-<button onClick={changeColor}
+<button onClick={() => changeColor('red')}
 className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'> test</button>
 
 <button className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'> test2</button>
