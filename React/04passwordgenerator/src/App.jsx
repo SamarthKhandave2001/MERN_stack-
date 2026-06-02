c7379b6136c37d0f01488a7b39e4e58c5c27906f
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-06-02 15:09:54 +0530

    Length add

--- React/04passwordgenerator/src/App.jsx
+++ React/04passwordgenerator/src/App.jsx
@@ -21,6 +21,22 @@ className = 'outline-none w-full py-1 px-3' placeholder='password' readOnly />
 
 <button className ='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0' >copy</button>
   </div>
+<div className='flex text-sm gap-x-2'>
+<div className='flex items-center gap-x-1'>
+  <input type="range"
+  min={6}
+  max={20}
+  value={length}
+  className= 'cursor-pointer '
+  onChange={(e) => setLength(e.target.value)}
+  name=""
+  id="" />
+<label htmlFor="length">Length: {length}</label>
+
+</div>
+
+</div>
+
   </div>
     </>
   )
