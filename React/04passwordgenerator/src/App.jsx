59f6031d081b9f5a5132ec8ee82281f534544e9f
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-06-02 14:50:55 +0530

    set password

--- React/04passwordgenerator/src/App.jsx
+++ React/04passwordgenerator/src/App.jsx
@@ -6,6 +6,9 @@ import './App.css'
 function App() {
   const [length, setLength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false)
+  const [symbolAllowed, setSymbolAllowed] = useState(false)
+  const[characterAllowed, setCharacterAllowed] = useState(false)
+  const [password, setPassword] = useState('')
 
   return (
 
