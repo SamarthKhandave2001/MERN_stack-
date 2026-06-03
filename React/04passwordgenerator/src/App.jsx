026b547e50a6e66a84586b3d2aaec6ae987ce0dd
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-06-03 15:46:34 +0530

    char

--- React/04passwordgenerator/src/App.jsx
+++ React/04passwordgenerator/src/App.jsx
@@ -20,8 +20,11 @@ function App() {
       if(characterAllowed) str += "abcdefghijklmnopqrstuvwxyz"
       for(let i = 1; i < length; i++){
         const char = math.floor(math.random() * str.length+1)
+        pass += str.charAt(char)
       }
-  })
+
+      setPassword(pass)
+  }, [length, numberAllowed, symbolAllowed, characterAllowed])
   
 
   return (
