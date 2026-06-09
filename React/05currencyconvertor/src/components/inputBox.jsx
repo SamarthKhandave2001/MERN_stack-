a2636dfe843e04331959ee5c1b57f7dafe657179
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-06-09 15:30:57 +0530

    new folder components

---
+++ React/05currencyconvertor/src/components/inputBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const InputBox  = () => {
+    return (
+        <div>InputBox</div>
+    )
+}
+
+const mapStateToProps = (state) => ({})
+
+const mapDispatchToProps = (dispatch) => ({})
+export default connect(mapStateToProps, mapDispatchToProps)(InputBox)
