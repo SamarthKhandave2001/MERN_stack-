434825eae245b674ef7d253c227add2633bb5cd4
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-06-10 12:25:40 +0530

    curencies

--- React/05currencyconvertor/src/components/inputBox.jsx
+++ React/05currencyconvertor/src/components/inputBox.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 function InputBox({
     label,
     amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencies = []
  } )  {
     return(
         <div>InputBox</div>
