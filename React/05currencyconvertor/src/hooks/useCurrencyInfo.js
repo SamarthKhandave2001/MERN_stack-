1b2865fdc83f1769d492d8e79ef8217d80afb36d
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-06-11 12:38:51 +0530

    Fetch currency

--- React/05currencyconvertor/src/hooks/useCurrencyInfo.js
+++ React/05currencyconvertor/src/hooks/useCurrencyInfo.js
@@ -1,7 +1,17 @@
 import {useEffect, useState} from 'react';
 
 
-function useCurrencyInfo() {}
+function useCurrencyInfo(currency) {
+
+const [data, setData] = useState({});
+
+useEffect(() => {
+fetch(`https://cdn. jsdelivr.net/gh/fawazahmed0/
+currency-api@1/latest/currencies/usd. json`)
+}, [currency]);
+
+
+}
 
 
 
