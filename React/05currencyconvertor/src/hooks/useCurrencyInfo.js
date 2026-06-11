dd5a39abfb7632ed4c12d293c7c37146b320b891
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-06-11 12:41:38 +0530

    updated usd

--- React/05currencyconvertor/src/hooks/useCurrencyInfo.js
+++ React/05currencyconvertor/src/hooks/useCurrencyInfo.js
@@ -7,7 +7,9 @@ const [data, setData] = useState({});
 
 useEffect(() => {
 fetch(`https://cdn. jsdelivr.net/gh/fawazahmed0/
-currency-api@1/latest/currencies/${currency}. json`)
+currency-api@1/latest/currencies/${currency}.json`)
+.then(res => res.json())
+.then(data => setData(res[usd]))
 }, [currency]);
 
 
