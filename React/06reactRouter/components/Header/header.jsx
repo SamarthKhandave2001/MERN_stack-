dd9d3eadf44fc6681c149da710d272864fcd5d47
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-06-18 15:55:10 +0530

    updated the header

--- React/06reactRouter/components/Header/header.jsx
+++ React/06reactRouter/components/Header/header.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 function Header() {
     return (
-        <div>Header</div>
+        <Header className='shadow sticky z-50 top-0;'>
+<nav className='bg-white border-grey-200 px-4 lg:px-6 py-2.5'>
+<div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
+</div>
+
+    </nav>
+
+
+        </Header>
     )
 }
 export default Header;
