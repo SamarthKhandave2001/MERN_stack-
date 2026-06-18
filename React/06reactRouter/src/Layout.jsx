c0db5ba4ba326a5b47ff245d873287cd9b236a00
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-06-18 12:24:15 +0530

    updated imports

--- React/06reactRouter/src/Layout.jsx
+++ React/06reactRouter/src/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import{ Outlet } from 'react-router-dom';
-import Header from './components/Header/header';
-import Footer from './Footer/Footer';
+import Header from '../components/Header/header';
+import Footer from '../Footer/Footer';
 
 
 
--- React/06reactRouter/src/main.jsx
+++ React/06reactRouter/src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Layout from './Layout.jsx'
 import App from './App.jsx'
 import './index.css'
+//import { createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+  
+    </Route>
+  )
+)
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
