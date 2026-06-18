f8c28edb6d88f864debad17a4a3fbc656b372ada
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-06-18 12:00:13 +0530

    imported header and footer

--- React/06reactRouter/Layout.jsx
+++ React/06reactRouter/Layout.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import{ Outlet } from 'react-router-dom';
+import Header from './components/Header/header';
+import Footer from './Footer/Footer';
+
+
+
 function Layout(){
     return(
+        <>
         <div>Layout</div>
+        <Header/>
+        <Footer/>
+
+
+
+
+        </>
     )
 }
 
