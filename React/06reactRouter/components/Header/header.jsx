40115b319b4a26a84eabf26c46db128e356c0b59
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-06-19 15:05:03 +0530

    updated header

--- React/06reactRouter/components/Header/header.jsx
+++ React/06reactRouter/components/Header/header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return (
         <Header className='shadow sticky z-50 top-0;'>
             <nav className='bg-white border-grey-200 px-4 lg:px-6 py-2.5'>
-                <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
+                <div className='flex flex-wrap justify-between items-center mx-auto'>
 
                     <Link to="/" className='flex items-center'>
 
