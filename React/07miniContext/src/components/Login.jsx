eb31873a23c3040f1fb94837c7987e6a11b5c7da
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-06-30 12:46:21 +0530

    Updated in Login

--- React/07miniContext/src/components/Login.jsx
+++ React/07miniContext/src/components/Login.jsx
@@ -2,8 +2,35 @@ import React from 'react';
 
 
 function Login(){
+const [username, setUsername] = React.useState('');
+const [password, setPassword] = React.useState('');
+
+const handleSubmit = (e) => {
+
+
+
+
+}
+
     return(
-        <div>Login</div>
+        <div>
+<h2>Login</h2>
+<input type="text"
+value={username}
+onChange={(e) => setUsername(e.target.value)}
+placeholder="Username" />
+
+{" "}
+
+<input type="password"
+value={password}
+onChange={(e) => setPassword(e.target.value)}
+placeholder="Password" />
+
+<button onClick={handleSubmit}>Submit</button>
+
+
+        </div>
     )
 }
 
