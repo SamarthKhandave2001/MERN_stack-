0289813b8bc34fd5f5f9797755e8d6957225530f
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-07-01 18:26:07 +0530

    Changs in user context

--- React/07miniContext/src/context/UserContextProvider.jsx
+++ React/07miniContext/src/context/UserContextProvider.jsx
@@ -1,22 +1,14 @@
-import React, {useState} from 'react';
-
+import React, { useState } from 'react';
 import UserContext from './UserContext';
 
-const UserContextProvider = ({childern}) => {
-const [user, setUser] = useState('null')
+const UserContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
 
-return (
-    <UserContext.Provider value={{user, setUser}}>
-
-        {childern}
-    </UserContext.Provider>
-)
-
-
-
-}
-
-
-
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
 
 export default UserContextProvider;
