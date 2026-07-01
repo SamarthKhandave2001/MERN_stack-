5813d06f347841e33ea86ef368bf6e466db66cdb
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-07-01 12:48:18 +0530

    User context

--- React/07miniContext/src/context/UserContextProvder.jsx
+++ React/07miniContext/src/context/UserContextProvder.jsx
@@ -1,3 +1,22 @@
 import React, {UseState} from 'react';
 
 import UserContext from './UserContext';
+
+const UserContextProvider = ({childern}) => {
+const [user, setUser] = UseState('null')
+
+return (
+    <UserContext.Provider >
+
+        {childern}
+    </UserContext.Provider>
+)
+
+
+
+}
+
+
+
+
+export default UserContextProvider;
