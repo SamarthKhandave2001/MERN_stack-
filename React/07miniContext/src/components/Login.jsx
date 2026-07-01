d9d3eeff7fcc1247a0eb29af5c906dc3c22cf9f4
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-07-01 18:22:13 +0530

    Updated

--- React/07miniContext/src/components/Login.jsx
+++ React/07miniContext/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React,{useState, useContext} from 'react';
-
+import UserContext from '../context/UserContext';
 
 function Login(){
 const [username, setUsername] = React.useState('');
--- React/07miniContext/src/context/UserContextProvider.jsx
+++ React/07miniContext/src/context/UserContextProvider.jsx
@@ -1,14 +1,22 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
+
 import UserContext from './UserContext';
 
-const UserContextProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+const UserContextProvider = ({childern}) => {
+const [user, setUser] = useState('null')
 
-    return (
-        <UserContext.Provider value={{ user, setUser }}>
-            {children}
-        </UserContext.Provider>
-    );
-};
+return (
+    <UserContext.Provider value={{user, setUser}}>
+
+        {childern}
+    </UserContext.Provider>
+)
+
+
+
+}
+
+
+
 
 export default UserContextProvider;
