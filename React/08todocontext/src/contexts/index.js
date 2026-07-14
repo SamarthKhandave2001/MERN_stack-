387903e0c865cbe0e06e78dc614ec1250e34aae0
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-07-14 15:01:05 +0530

    Updated todo

--- React/08todocontext/src/contexts/index.js
+++ React/08todocontext/src/contexts/index.js
@@ -4,11 +4,18 @@ import {createContext, useContext} from "react"
     export const TodoContext = createContext({
 todos: [
 
-    {
+    { 
+        id:1,
+        todo: "Learn React",
+        completed: false
 
-        
+
     }
-]
+],
+
+addTodo: (todo) => {},
+updateTodo: (id, todo) => {},
+
 
 
 
