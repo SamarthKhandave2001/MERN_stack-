7918b0c23dc6ab817ace3bdd99bac6950c0c99f0
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-07-15 15:35:22 +0530

    Import todo provider

--- React/08todocontext/src/App.jsx
+++ React/08todocontext/src/App.jsx
@@ -2,19 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TodoProvider } from './context/TodoContext'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <TodoProvider>
      <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
    
     <input type="text" placeholder='Enter your task' />
     <input type="submit" value="Add Task" />
-    </>
+    </TodoProvider>
   )
 }
 
