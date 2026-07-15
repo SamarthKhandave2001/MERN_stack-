0678da3481f840d63fb49d60f718f3b5a06614a3
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-07-15 15:52:05 +0530

    Delete Todo done

--- React/08todocontext/src/App.jsx
+++ React/08todocontext/src/App.jsx
@@ -3,12 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { TodoProvider } from './context/TodoContext'
+import {useTodo} from './context/TodoContext'
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+ const addTodo = (todo) => {
+  setTodos((prevTodos) => [{id: Date.now(), ...todo},    ...prevTodos])
+ }
+
+ const updateTodo = (id, Todo) => {
+setTodos((prev) => prev.map((prevTodo) => (prevTodo.id===todo.id ? {...prevTodo, ...Todo} : prevTodo)))
+
+ }
+
+ const deleteTodo = (id) => {
+  setTodos((prev) => prev.filter((todo) => todo.id !== id))
+ }
+
 
   return (
-    <TodoProvider>
+    <TodoProvider values = {{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
      <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
