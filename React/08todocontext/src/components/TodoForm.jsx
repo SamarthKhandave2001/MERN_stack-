8444faac36b6e8263a43aceb06f3a8b75a2aedd2
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-07-20 15:39:52 +0530

    comm

--- React/08todocontext/src/components/TodoForm.jsx
+++ React/08todocontext/src/components/TodoForm.jsx
@@ -18,7 +18,7 @@ const add = (e) => {
 <form onSubmit={add} className ='flex'>
 <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} placeholder='Write Todo...' className='W-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white 20 py-1.5'/>
 
-<button type='submit' className='bg-blue-500 text-white px-3 py-1.5 rounded-r-lg hover:bg-blue-600 duration-150'>Add</button>
+<button type='submit' className='bg-blue-500 text-white px-3 py-1.5 rounded-r-lg hover:bg-blue-600 duration-150'>Add</button>  //add button to submit the form and add a new todo item
 
 </form>
 
